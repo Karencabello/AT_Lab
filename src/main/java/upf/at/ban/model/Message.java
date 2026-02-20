709e79ba4c349474e4dbd2fc976bd2b2de48ee30
Karencabello <karencabellogonzalez@gmail.com>
2026-02-20 13:12:48 +0100

    XXXXX

---
+++ src/main/java/upf/at/ban/model/Message.java
@@ -0,0 +1,36 @@
+package upf.at.ban.model;
+
+public class Message {
+    private long chat_id;
+    private String text;
+
+    public Message() { }
+
+    public Message(long chat_id, String text) {
+        this.chat_id = chat_id;
+        this.text = text;
+    }
+
+
+    public long getChat_id() {
+        return chat_id;
+    }
+
+    public void setChat_id(long chat_id) {
+        this.chat_id = chat_id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return "Message [chat_id=" + chat_id + ", text=" + text + "]";
+    }
+
+}
--- src/main/java/upf/at/ban/service/TelegramService.java
+++ src/main/java/upf/at/ban/service/TelegramService.java
@@ -1,1 +1,19 @@
 package upf.at.ban.service;
+
+import upf.at.ban.model.Message;
+import upf.at.ban.util.HttpClientUtil;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+
+public class TelegramService {
+
+    private static final String TELEGRAM_API_URL = "https://api.telegram.org/bot";
+
+    public void sendMessage(String token, Message message) {
+        String url = TELEGRAM_API_URL + token + "/sendMessage";
+        HttpClientUtil.sendPost(url, message);
+    }
+}
