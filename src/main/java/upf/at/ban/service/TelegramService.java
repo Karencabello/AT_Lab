ecfa220f2685eb6299199b06de7bdea0edb99aa8
Karencabello <karencabellogonzalez@gmail.com>
2026-02-20 18:37:50 +0100

    telegram

--- src/main/java/upf/at/ban/service/TelegramService.java
+++ src/main/java/upf/at/ban/service/TelegramService.java
@@ -1,19 +1,38 @@
 package upf.at.ban.service;
 
 import upf.at.ban.model.Message;
-import upf.at.ban.util.HttpClientUtil;
+import upf.at.ban.util.Constants;
 
 import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 
+
 public class TelegramService {
 
-    private static final String TELEGRAM_API_URL = "https://api.telegram.org/bot";
+    // Enviar missatge a Telegram
+    // RECORDATOR: el bot no rep missatges com un endpoint, sinó que el client ha de fer una petició POST a Telegram amb el missatge i el token del bot. Telegram s'encarrega d'entregar el missatge al bot.
+    public String sendMessage(String token, long chatId, String text){ 
+        
+        // 1. Creem missatge
+        Message message = new Message(chatId, text);
+        
+        // 2. Creem client
+        Client client = ClientBuilder.newClient();
 
-    public void sendMessage(String token, Message message) {
-        String url = TELEGRAM_API_URL + token + "/sendMessage";
-        HttpClientUtil.sendPost(url, message);
+        // 3. Creem URL
+        WebTarget targetSendMessage = client.target(Constants.TELEGRAM_API_URL + Constants.TELEGRAM_TOKEN).path("sendMessage");
+
+        // 4. Fem petició POST
+        String response = targetSendMessage.request(MediaType.APPLICATION_JSON_TYPE)
+            .post(Entity.entity(message, MediaType.APPLICATION_JSON_TYPE), String.class);
+
+        // 5. Tan quem client
+        client.close();
+        
+        return response;
     }
 }
+
--- src/main/java/upf/at/ban/util/Constants.java
+++ src/main/java/upf/at/ban/util/Constants.java
@@ -6,6 +6,11 @@ public class Constants{
     public static final String BICING_PATH = "6aa3416d-ce1a-494d-861b-7bd07f069600/resource/1b215493-9e63-4a12-8980-2d7e0fa19f85/download";
     public static final String BICING_TOKEN = "cb98b0c4b326da635573050e787fb0ca30f5ee426d70e92c3e6f9d5808f67cf8";
 
+    //Telegram
+    public static final String TELEGRAM_API_URL = "https://api.telegram.org/bot";   
+    public static final String TELEGRAM_TOKEN = "8375476740:AAF16doZ1TCzUBtSSuV4u6xTOK8Od6YUPN4";
+
+
     //Cache
     // Temps vida cache --> 120s en ms
     public static final long CACHE_TTL = 120_000;
